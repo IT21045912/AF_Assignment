fa5cc694f49fc6ac4b44788069c680e84f19b015
IT21031120 <it21031120@my.sliit.lk>
2023-05-05 14:52:52 +0530

    Navbar change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Home from './pages/Home';
-import { Route,  Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Register from './pages/User/Register';
 import Login from './pages/User/Login';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
-        <Route exact path="/" element ={<Home/>}/>
-        <Route exact path="/Register" element ={<Register/>}/>
-        <Route exact path="/Login" element ={<Login/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Register" element={<Register />} />
+        <Route exact path="/Login" element={<Login />} />
       </Routes>
     </>
   );
