325b621c037dd5afa0552b2b29fc058882560f9b
Sankalpa2000 <Sankalpa.pasindu7@gmail.com>
2023-05-05 07:26:47 +0530

    nothing

---
+++ frontend/src/pages/User/FarmerRegister.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+function FarmerRegister() {
+  return (
+    <Container
+      fluid
+      style={{
+        marginTop: "10%",
+        display: "block",
+        width: "50%",
+        justifyContent: "center",
+      }}
+    >
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name :</Form.Label>
+          <Form.Control type="Name" placeholder="Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Address :</Form.Label>
+          <Form.Control type="Address" placeholder="Address" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Phone :</Form.Label>
+          <Form.Control type="Phone" placeholder="Phone" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="Password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default FarmerRegister
